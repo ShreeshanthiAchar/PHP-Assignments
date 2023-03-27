1027527bc94918c9d4ba83908d9552241385cd1a
ShreeshanthiAchar <124127662+ShreeshanthiAchar@users.noreply.github.com>
2023-03-27 14:35:47 +0530

    Add files via upload

---
+++ phpSecondAssignment/Regform.php
@@ -0,0 +1,23 @@
+<!DOCTYPE HTML>
+<html>  
+<body>
+
+<h2>Registration Form</h2>
+
+
+<form action="welcome.php" method="post">
+Name: <input type="text" name="name"><br><br>
+E-mail: <input type="text" name="email"><br><br>
+Phone-no: <input type="tel" name="phone" maxlength="10"><br><br>
+Message: <textarea name="Message" rows="3" cols="40"></textarea>
+  <br><br>
+Gender:
+  <input type="radio" name="gender"  value="female">Female
+  <input type="radio" name="gender"  value="male">Male
+  <input type="radio" name="gender"  value="other">Other  
+  <br><br>
+<input type="submit">
+</form>
+
+</body>
+</html>
