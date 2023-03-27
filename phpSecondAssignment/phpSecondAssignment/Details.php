f91589cbc1ef39e33089f0f768931fe05cefa981
ShreeshanthiAchar <124127662+ShreeshanthiAchar@users.noreply.github.com>
2023-03-27 14:33:45 +0530

    Add files via upload

---
+++ phpSecondAssignment/phpSecondAssignment/Details.php
@@ -0,0 +1,6 @@
+<h2>Your Details:</h2>
+Name:<?php echo $_POST["name"]; ?><br>
+Email:<?php echo $_POST["email"]; ?><br>
+Phone:<?php echo $_POST["phone"]; ?><br>
+Gender:<?php echo $_POST["gender"]; ?><br>
+Your Message:<?php echo $_POST["Message"]; ?><br>
