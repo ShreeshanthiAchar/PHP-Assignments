1361637bcbd25d78d9f8dac5bbbcc56040fefb57
ShreeshanthiAchar <124127662+ShreeshanthiAchar@users.noreply.github.com>
2023-04-18 10:27:34 +0530

    Add files via upload

---
+++ phpSixthAssignment/connection2.php
@@ -0,0 +1,11 @@
+<?php      
+    $host = "localhost";  
+    $user = "shreeshanthi";  
+    $password = '';  
+    $db_name = "shreeshanthi_form";  
+      
+    $con = mysqli_connect("localhost", "shreeshanthi", "shreeshanth1", "shreeshanthi_form");  
+    if(mysqli_connect_errno()) {  
+        die("Failed to connect with MySQL: ". mysqli_connect_error());  
+    }  
+?>  
