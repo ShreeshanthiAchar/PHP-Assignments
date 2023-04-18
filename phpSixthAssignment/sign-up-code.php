978fa01d32215802d8e2ead5b8e63bca6a975da4
ShreeshanthiAchar <124127662+ShreeshanthiAchar@users.noreply.github.com>
2023-04-18 10:35:23 +0530

    Add files via upload

---
+++ phpSixthAssignment/sign-up-code.php
@@ -0,0 +1,13 @@
+<?php
+$connection = mysqli_connect("localhost", "shreeshanthi", "shreeshanth1", "shreeshanthi_form");
+    
+// Check connection
+if(!$connection){
+    die('Could not Connect MySql Server:' .mysql_error());
+}
+else
+{
+    echo "";
+}
+
+?>
